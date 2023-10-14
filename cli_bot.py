db6d57fec25cc74f6989f0394e86683cca39a47c
spogoretskyi <spogoretskyi@gmail.com>
2023-10-14 12:42:03 +0300

    small update

--- cli_bot.py
+++ cli_bot.py
@@ -4,6 +4,10 @@ def input_error(func):
             return func(*args, **kwargs)
         except ValueError:
             return "Give me name and phone please."
+        except KeyError:
+            return "No such contact in the list."
+        except IndexError:
+            return "Give me name and phone please."
 
     return inner
 
